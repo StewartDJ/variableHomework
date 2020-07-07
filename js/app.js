92cf567563b4e89927c7a3507ee154ffae7ff9fb
DJ Stewart <deshastewart1992@gmail.com>
2020-07-06 17:05:52 -0700

    second commit

---
+++ js/app.js
@@ -0,0 +1,63 @@
+var variableOne = 1;
+var variableTwo = 2;
+var variableThree = 3;
+var variableFour = 4;
+var variableFive = 5;
+var variableSix = 6;
+var variableSeven = 7;
+var variableEight = 8;
+var variableNine = 9;
+var variableTen = 10;
+
+//First Addition of Variables 
+var myVar = (variableNine + variableTen);
+console.log(myVar);
+
+//Second Addition of Variables 
+var myVar = (variableFive + variableFive);
+console.log(myVar);
+
+// First Subtraction of Variables
+var myVar = (variableFive- variableOne);
+console.log(myVar);
+
+// Second Subtraction of Variables 
+var myVar = (variableThree - variableTwo);
+console.log(myVar);
+
+
+// First Multiplication of Variables
+var myVar = (variableSeven * variableNine);
+console.log(myVar);
+
+
+// Second Multiplication of Variables
+var myVar = (variableSix * variableFour);
+console.log(myVar);
+//Second Division of V
+
+// First Division of Variables
+var myVar = (variableTen / variableTwo);
+console.log(myVar);
+//Second Division of Variables
+ 
+var myVar = (variableEight / variableTwo);
+console.log(myVar);
+
+
+// first increment of variables
+variableTwo++;
+console.log(variableTwo);
+
+// second increment of variables
+variableEight++;
+console.log(variableEight);
+
+
+// Decrement A Number
+variableThree--;
+console.log(variableThree);
+
+// decrement one more number
+variableSeven--;
+console.log(variableSeven);
